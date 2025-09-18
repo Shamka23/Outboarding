9414998ae22e212961089e7cf657073ecec7f011
Shamil <www.shamil.com91@gmail.com>
2025-09-18 16:53:47 +0300

    Система управления IoT

---
+++ nice/day/bro/managementSystem/src/SmartLight.java
@@ -0,0 +1,17 @@
+public class SmartLight extends IoTDevice{
+    private boolean isOn;
+
+    public SmartLight(int id, String name, boolean isOn) {
+        super(id, name);
+        this.isOn = isOn;
+    }
+
+    @Override
+    public void performAction() {
+        if (isOn == true){
+            System.out.println("Умный свет включен");
+        } else {
+            System.out.println("Умный свет выключен");
+        }
+    }
+}
