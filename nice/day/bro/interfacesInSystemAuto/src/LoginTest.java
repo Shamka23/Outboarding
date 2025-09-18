193bc801a429c3c310b63ce1909d484d447f3705
Shamil <www.shamil.com91@gmail.com>
2025-09-18 17:59:27 +0300

    Interfaces if system of Automatization

---
+++ nice/day/bro/interfacesInSystemAuto/src/LoginTest.java
@@ -0,0 +1,6 @@
+public class LoginTest implements TestScenario{
+    @Override
+    public void runTest() {
+        System.out.println("Началось тестирование входа в систему");
+    }
+}
---
+++ nice/day/bro/interfacesInSystemAuto/src/TestAutomationSystem.java
@@ -0,0 +1,12 @@
+public class TestAutomationSystem {
+    public static void main(String[] args) {
+        TestScenario[] tests = new TestScenario[3];
+        tests[0] = new LoginTest();
+        tests[1] = new PaymentTest();
+        tests[2] = new ProductSearchTest();
+
+        for(TestScenario elements: tests){
+            elements.runTest();
+        }
+    }
+}
